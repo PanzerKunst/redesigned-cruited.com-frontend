2f203893e485e2882efecc632f2d53c2e00f51b5
Christophe Bram <cbramdit@gmail.com>
2015-09-11 10:36:38 +0200

    Extracting the markup of the front page

---
+++ wordpress/web/app/themes/cruited/front-page.php
@@ -0,0 +1,14 @@
+<?php
+// create a new cURL resource
+$ch = curl_init("https://raw.githubusercontent.com/PanzerKunst/redesigned-cruited.com-frontend/master/wordpress/web/app/themes/cruited/front-page.html");
+
+// set URL and other appropriate options
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+// grab URL and pass it to the browser
+curl_exec($ch);
+
+// close cURL resource, and free up system resources
+curl_close($ch);
+?>
