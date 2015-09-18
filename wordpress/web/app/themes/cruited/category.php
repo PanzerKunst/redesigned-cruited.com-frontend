bca20daffcbdfeb6ecde03aa1c10bca88d86fa74
Christophe Bram <cbramdit@gmail.com>
2015-09-18 16:13:57 +0200

    Improved meta on posts and breadcrumbs

--- wordpress/web/app/themes/cruited/category.php
+++ wordpress/web/app/themes/cruited/category.php
@@ -8,11 +8,7 @@
 <?php endif; ?>
 
 <div class="page-content with-circles">
-    <?php if (function_exists('yoast_breadcrumb')) {
-        yoast_breadcrumb('<p class="yoast-breadcrumbs">','</p>');
-    }
-
-    echo category_description(); ?>
+    <?= category_description(); ?>
     <?php while (have_posts()) : the_post(); ?>
         <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
     <?php endwhile; ?>
--- wordpress/web/app/themes/cruited/templates/page-header.php
+++ wordpress/web/app/themes/cruited/templates/page-header.php
@@ -3,5 +3,9 @@
 <section class="page-header img-bg no-img centered-contents">
     <div>
         <h1><?= Cruited\title(); ?></h1>
+        <?php
+        if (function_exists('yoast_breadcrumb') && substr_count($_SERVER['REQUEST_URI'], '/') > 2) {
+            yoast_breadcrumb('<p class="yoast-breadcrumbs">','</p>');
+        } ?>
     </div>
 </section>
