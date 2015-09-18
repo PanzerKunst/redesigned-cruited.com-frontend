feb41a43c286b12031794f11a4f1c90ade8b1edf
Christophe Bram <cbramdit@gmail.com>
2015-09-18 17:24:51 +0200

    Added "Läs mer" link to archive page

--- wordpress/web/app/themes/cruited/templates/content.php
+++ wordpress/web/app/themes/cruited/templates/content.php
@@ -1,10 +1,14 @@
-<?php use Roots\Sage\Cruited; ?>
+<?php
+use Roots\Sage\Cruited;
+use Roots\Sage\Extras;
+?>
 
 <article <?php post_class(); ?>>
   <header>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
   </header>
   <div class="entry-summary">
-    <?= Cruited\get_the_excerpt(); ?>
+      <p><?= Cruited\get_the_excerpt() ?></p>
+      <div><?= Extras\excerpt_more() ?></div>
   </div>
 </article>
