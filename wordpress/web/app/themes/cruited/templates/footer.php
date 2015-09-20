d267840ee6ba95ab3d8c3fd13650d7102b42513d
forsandree <efa@efa.nu>
2015-09-20 20:44:27 +0200

    Change in guarantee

--- wordpress/web/app/themes/cruited/templates/footer.php
+++ wordpress/web/app/themes/cruited/templates/footer.php
@@ -6,7 +6,7 @@
 
                 <a href="/users/select-document" class="btn btn-primary">Starta din granskning</a>
 
-                <a href="/#faq">100% nöjd - eller pengarna tillbaka - Leverans inom 24 timmar</a>
+                <a href="/#faq">100% nöjd - eller pengarna tillbaka<br />Svar inom 24 timmar</a>
             </div>
         </section>
 
