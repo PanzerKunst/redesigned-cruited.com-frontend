3e6620db2e45dde0b3a549044448ec47de73b262
Christophe Bram <cbramdit@gmail.com>
2015-11-20 15:43:40 +0100

    [WP] Added social icons in footer

--- wordpress/web/app/themes/cruited/templates/footer.php
+++ wordpress/web/app/themes/cruited/templates/footer.php
@@ -1,20 +1,27 @@
 <footer class="content-info" role="contentinfo">
-    <div class="container">
-        <section id="bottom-hero" class="img-bg centered-contents">
-            <div>
-                <h2>Börja förbättra din jobbansökan nu!</h2>
+  <div class="container">
+    <section id="bottom-hero" class="img-bg centered-contents">
+      <div>
+        <h2>Börja förbättra din jobbansökan nu!</h2>
 
-                <a href="/users/select-document/" class="btn btn-primary">Starta din granskning</a>
+        <a href="/users/select-document/" class="btn btn-primary">Starta din granskning</a>
 
-                <a href="/#faq">100% nöjd - eller pengarna tillbaka<br />Svar inom 24 timmar</a>
-            </div>
-        </section>
+        <a href="/#faq">100% nöjd - eller pengarna tillbaka<br/>Svar inom 24 timmar</a>
+      </div>
+    </section>
 
-        <section id="footer-proper">
-            <h2 class="brand">Cruited</h2>
-            <div>
-                <?php dynamic_sidebar('sidebar-footer'); ?>
-            </div>
-        </section>
-    </div>
+    <section id="footer-proper">
+      <section id="brand-and-social-icons">
+        <h2 class="brand">Cruited</h2>
+        <div>
+          <a class="social-icon facebook" href="https://www.facebook.com/Cruited"></a>
+          <a class="social-icon linkedin" href="https://www.linkedin.com/company/cruited"></a>
+          <a class="social-icon twitter" href="https://twitter.com/cruited"></a>
+        </div>
+      </section>
+      <section id="widgets">
+        <?php dynamic_sidebar('sidebar-footer'); ?>
+      </section>
+    </section>
+  </div>
 </footer>
