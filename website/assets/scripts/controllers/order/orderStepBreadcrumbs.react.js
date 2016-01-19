865016315f4c3e05abd9cd4364fe5bab26946f5a
Christophe Bram <cbramdit@gmail.com>
2016-01-19 14:02:16 +0100

    [App] On the order pages, the pills for the done steps are now styled differently than those for the current & future steps. Also, I deleted the link on the pill for the current step.

--- website/assets/scripts/controllers/order/orderStepBreadcrumbs.react.js
+++ website/assets/scripts/controllers/order/orderStepBreadcrumbs.react.js
@@ -2,33 +2,57 @@
 
 CR.Controllers.OrderStepBreadcrumbs = React.createClass({
     render: function() {
-        let stepTwoInnerTag = "<span>2. " + CR.i18nMessages["order.assessmentInfo.title"] + "</span>";
-        let stepTwoTag = (<span dangerouslySetInnerHTML={{__html: stepTwoInnerTag}} />);
-        if (this.props.step >= CR.Controllers.OrderCommon.steps.assessmentInfo) {
-            stepTwoTag = (<a href="/order/assessment-info" dangerouslySetInnerHTML={{__html: stepTwoInnerTag}} />);
-        }
-
-        let stepThreeInnerTag = "<span>3. " + CR.i18nMessages["order.accountCreation.title"] + "</span>";
-        let stepThreeTag = (<span dangerouslySetInnerHTML={{__html: stepThreeInnerTag}} />);
-        if (this.props.step >= CR.Controllers.OrderCommon.steps.accountCreation) {
-            stepThreeTag = (<a href="/order/create-account" dangerouslySetInnerHTML={{__html: stepThreeInnerTag}} />);
-        }
-
-        let stepFourInnerTag = "<span>4. " + CR.i18nMessages["order.payment.title"] + "</span>";
-        let stepFourTag = (<span dangerouslySetInnerHTML={{__html: stepFourInnerTag}} />);
-        if (this.props.step >= CR.Controllers.OrderCommon.steps.payment) {
-            stepFourTag = (<a href="/order/payment" dangerouslySetInnerHTML={{__html: stepFourInnerTag}} />);
-        }
-
         return (
             <nav id="order-steps-breadcrumbs">
-                <a href="/order">
-                    <span>1. {CR.i18nMessages["order.productSelection.title"]}</span>
-                </a>
-                {stepTwoTag}
-                {stepThreeTag}
-                {stepFourTag}
+                {this._getStepOneTag()}
+                {this._getStepTwoTag()}
+                {this._getStepThreeTag()}
+                {this._getStepFourTag()}
             </nav>
         );
+    },
+
+    _getStepOneTag: function() {
+        let stepOneInnerTag = "<span>1. " + CR.i18nMessages["order.productSelection.title"] + "</span>";
+        let stepOneTag = <a href="/order" dangerouslySetInnerHTML={{__html: stepOneInnerTag}} />;
+        if (this.props.step === CR.Controllers.OrderCommon.steps.productSelection) {
+            stepOneTag = <span dangerouslySetInnerHTML={{__html: stepOneInnerTag}} className="current-step" />;
+        }
+
+        return stepOneTag;
+    },
+
+    _getStepTwoTag: function() {
+        let stepTwoInnerTag = "<span>2. " + CR.i18nMessages["order.assessmentInfo.title"] + "</span>";
+        let stepTwoTag = <a href="/order/assessment-info" dangerouslySetInnerHTML={{__html: stepTwoInnerTag}} />;
+        if (this.props.step === CR.Controllers.OrderCommon.steps.assessmentInfo) {
+            stepTwoTag = <span dangerouslySetInnerHTML={{__html: stepTwoInnerTag}} className="current-step" />;
+        } else if (this.props.step < CR.Controllers.OrderCommon.steps.assessmentInfo) {
+            stepTwoTag = <span dangerouslySetInnerHTML={{__html: stepTwoInnerTag}} />;
+        }
+
+        return stepTwoTag;
+    },
+
+    _getStepThreeTag: function() {
+        let stepThreeInnerTag = "<span>2. " + CR.i18nMessages["order.accountCreation.title"] + "</span>";
+        let stepThreeTag = <a href="/order/create-account" dangerouslySetInnerHTML={{__html: stepThreeInnerTag}} />;
+        if (this.props.step === CR.Controllers.OrderCommon.steps.accountCreation) {
+            stepThreeTag = <span dangerouslySetInnerHTML={{__html: stepThreeInnerTag}} className="current-step" />;
+        } else if (this.props.step < CR.Controllers.OrderCommon.steps.accountCreation) {
+            stepThreeTag = <span dangerouslySetInnerHTML={{__html: stepThreeInnerTag}} />;
+        }
+
+        return stepThreeTag;
+    },
+
+    _getStepFourTag: function() {
+        let stepFourInnerTag = "<span>2. " + CR.i18nMessages["order.payment.title"] + "</span>";
+        let stepFourTag = <span dangerouslySetInnerHTML={{__html: stepFourInnerTag}} />;
+        if (this.props.step === CR.Controllers.OrderCommon.steps.payment) {
+            stepFourTag = <span dangerouslySetInnerHTML={{__html: stepFourInnerTag}} className="current-step" />;
+        }
+
+        return stepFourTag;
     }
 });
