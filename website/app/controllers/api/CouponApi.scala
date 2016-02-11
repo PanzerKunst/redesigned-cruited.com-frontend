a583a6c7c3a5380973792277369de1711ff4cc4b
Christophe Bram <cbramdit@gmail.com>
2016-02-11 11:45:20 +0100

    [App] Now logging usage of incorrect or expired coupons

--- website/app/controllers/api/CouponApi.scala
+++ website/app/controllers/api/CouponApi.scala
@@ -3,6 +3,7 @@ package controllers.api
 import javax.inject.Singleton
 
 import db.CouponDto
+import play.api.Logger
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
 import services.{CouponService, HttpService, SessionService}
@@ -11,7 +12,9 @@ import services.{CouponService, HttpService, SessionService}
 class CouponApi extends Controller {
   def get(code: String) = Action { request =>
     CouponDto.getOfCode(code) match {
-      case None => NoContent
+      case None =>
+        Logger.info("Someone tried to use the following incorrect or expired coupon: '" + code + "'")
+        NoContent
       case Some(coupon) =>
         if (CouponService.hasReachedMaxUses(coupon, SessionService.getAccountId(request.session))) {
           Status(HttpService.httpStatusCouponHasReachedMaxUses)
