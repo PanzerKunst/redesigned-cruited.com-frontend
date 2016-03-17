51796f994aed42fcefea4542d44001d9ad46f80e
Christophe Bram <cbramdit@gmail.com>
2016-03-17 16:07:24 +0100

    [WP] Added footer for US site

---
+++ wordpress/web/app/themes/cruited/templates/footer-us.php
@@ -0,0 +1,27 @@
+<footer class="content-info" role="contentinfo">
+  <div class="container">
+    <section id="bottom-hero" class="img-bg centered-contents">
+      <div>
+        <h2>Börja förbättra din jobbansökan nu!</h2>
+
+        <a href="https://app.cruited.com/order" class="btn btn-primary">Starta din granskning</a>
+
+        <a href="/#faq">100% nöjd - eller pengarna tillbaka<br/>Svar inom 24 timmar</a>
+      </div>
+    </section>
+
+    <section id="footer-proper">
+      <section id="brand-and-social-icons">
+        <h2 class="brand">Cruited</h2>
+        <div>
+          <a class="social-icon facebook" href="https://www.facebook.com/Cruited"></a>
+          <a class="social-icon linkedin" href="https://www.linkedin.com/company/cruited"></a>
+          <a class="social-icon twitter" href="https://twitter.com/cruited"></a>
+        </div>
+      </section>
+      <section id="widgets">
+        <?php dynamic_sidebar('sidebar-footer'); ?>
+      </section>
+    </section>
+  </div>
+</footer>
