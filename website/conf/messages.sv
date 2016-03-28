64fa475dd384a5e37f100acc3704fa898c0288b5
Christophe Bram <cbramdit@gmail.com>
2016-03-28 11:06:13 +0200

    [App] Changed info messages indicating to sign in with LI first

--- website/conf/messages.sv
+++ website/conf/messages.sv
@@ -213,7 +213,7 @@ order.assessmentInfo.validation.linkedin.incompleteProfile.latestProfessionalExp
 order.assessmentInfo.validation.jobAdUrlIncorrect=Den adressen ser inte rätt ut :(
 order.assessmentInfo.validation.customerCommentTooLong=Högst 512 tecken
 order.assessmentInfo.validation.requestEntityTooLarge=Filerna får inte vara större än 10MB totalt
-order.assessmentInfo.validation.signInWithLinkedinFirst=Logga in med LinkedIn först
+order.assessmentInfo.validation.signInWithLinkedinFirst=Börja med LinkedIn inloggning, tack!
 
 order.assessmentInfo.form.browseBtn.text=Bläddra...
 order.assessmentInfo.form.linkedinProfile.label=Din LinkedIn-profil
