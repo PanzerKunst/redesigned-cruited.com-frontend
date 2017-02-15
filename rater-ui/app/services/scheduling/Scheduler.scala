7f4b110d766b625731b3d69e808f207397cf8f8c
Christophe Bram <cbramdit@gmail.com>
2017-02-15 17:20:28 +0100

    [RA] Fixed the bug where the scheduler was hanging if an exception was encountered

--- rater-ui/app/services/scheduling/Scheduler.scala
+++ rater-ui/app/services/scheduling/Scheduler.scala
@@ -6,6 +6,7 @@ import javax.inject.{Inject, Singleton}
 
 import db.OrderDto
 import models.Order
+import play.api.Logger
 import play.api.i18n.MessagesApi
 
 @Singleton
@@ -15,9 +16,13 @@ class Scheduler @Inject()(orderDto: OrderDto, emailService: EmailService, messag
 
   val task = new Runnable {
     def run() = {
-      calculateOrdersSentToTheCustomer()
-      orderDto.calculateOrdersToDo()
-      handleScheduledAssessementsArrivedToTerm()
+      try {
+        calculateOrdersSentToTheCustomer()
+        orderDto.calculateOrdersToDo()
+        handleScheduledAssessementsArrivedToTerm()
+      } catch {
+        case t: Throwable => Logger.error(t.getMessage, t)
+      }
     }
   }
 
